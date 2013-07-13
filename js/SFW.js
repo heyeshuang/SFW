111c08070831e7d69c3ac907448282089000ecf9
heyeshuang <yeshuanghe@gmail.com>
2013-07-14 01:03:53 +0800

    drop jQuery for compatibility reasons. Work,Work never ends.

--- js/SFW.js
+++ js/SFW.js
@@ -1,67 +1,50 @@
 (function() {
-  var alarmBox, configBox, fridgeMagnet, jW, pulseClock, pulseCount, timeForRelax, timeoutAlarm;
-
-  jW = jQuery.noConflict();
+  var alarmBox, configBox, divToAppend, fridgeMagnet, pulseClock, timeForRelax;
 
   fridgeMagnet = '<div class="pinned">\n  <a id="boxOpen" href="javascript:;">Click Me</a>\n</div>';
 
-  alarmBox = '<div id="alarmBox" class="invisible" >\n  <h6>time for work babe</h6>\n  <a id="boxCloseNG" href="javascript:;">OK</a>\n</div>';
+  alarmBox = '<div id="alarmBox">\n  <h6>time for work babe</h6>\n  <a id="boxCloseNG" href="javascript:;">OK</a>\n</div>';
 
-  configBox = '<div id="configBox" class="invisible" >\n  <h6>close me</h6>\n  <a id="boxClose" href="javascript:;">OK</a>\n  <a id="setClock" href="javascript:;">I wanna work,but not now</a>\n</div>';
+  configBox = '<div id="configBox">\n  <h6>close me</h6>\n  <a id="boxClose" href="javascript:;">OK</a>\n  <a id="setClock" href="javascript:;">I wanna work,but not now</a>\n</div>';
 
-  jW("body").append(fridgeMagnet);
+  divToAppend = document.createElement("div");
 
-  jW("body").append(alarmBox);
+  divToAppend.innerHTML = fridgeMagnet;
 
-  jW("body").append(configBox);
+  document.body.appendChild(divToAppend);
 
   timeForRelax = 10;
 
   pulseClock = 0;
 
-  pulseCount = function() {
-    timeForRelax -= 1;
-    clearTimeout(pulseClock);
-    jW("#boxOpen").html(timeForRelax);
-    if (timeForRelax === 0) {
-      return timeoutAlarm();
-    } else {
-      return pulseClock = setTimeout(pulseCount, 1000);
-    }
-  };
-
-  timeoutAlarm = function() {
+  document.getElementById("boxOpen").onclick = function() {
     return easyDialog.open({
-      container: "alarmBox"
+      container: {
+        header: "hello",
+        content: configBox,
+        noFn: true
+      }
     });
   };
-
-  jW("#boxOpen").click(function() {
-    return easyDialog.open({
-      container: "configBox"
-    });
-  });
-
-  jW("#boxClose").click(function() {
-    return easyDialog.close();
-  });
-
-  jW("#setClock").click(function() {
-    pulseCount();
-    return easyDialog.close();
-  });
-
-  jW("#boxCloseNG").click(function() {});
 
 }).call(this);
 
 (function() {
-  var CSS, jW;
+  var cssContainer, cssContent, error;
 
-  jW = jQuery.noConflict();
+  cssContent = ' .pinned{\n\n   padding:0 10px;\n\n   line-height:50px;\n\n   opacity:0.7;\n\n   background: #FC6;\n\n   border:1px solid #F90;\n\n   position:fixed;\n\n   left:10px;\n\n   bottom:10px;\n\n   }\n .invisible{\n\n   display:none;\n\n   cursor: default;\n\n   }\nbutton::-moz-focus-inner{\nborder:0;\npadding:0;\nmargin:0;\n}\n\n.easyDialog_wrapper{\nwidth:320px;\ncolor:#444;\nborder:3px solid rgba(0,0,0,0);\n-webkit-border-radius:5px;\n-moz-border-radius:5px;\nborder-radius:5px;\n-webkit-box-shadow:0 0 10px rgba(0,0,0,0.4);\n-moz-box-shadow:0 0 10px rgba(0,0,0,0.4);\nbox-shadow:0 0 10px rgba(0,0,0,0.4);\ndisplay:none;\nfont-family:"Microsoft yahei", Arial;\n}\n\n.easyDialog_wrapper .easyDialog_content{\n-webkit-border-radius:4px;\n-moz-border-radius:4px;\nborder-radius:4px;\nbackground:#fff;\nborder:1px solid #e5e5e5;\n}\n\n.easyDialog_wrapper .easyDialog_title{\nheight:30px;\nline-height:30px;\noverflow:hidden;\ncolor:#666;\npadding:0 10px;\nfont-size:14px;\nborder-bottom:1px solid #e5e5e5;\nbackground:#f7f7f7;\nborder-radius:4px 4px 0 0;\n}\n\n.easyDialog_wrapper .close_btn{\nfont-family:arial;\nfont-size:18px;\n_font-size:12px;\nfont-weight:700;\ncolor:#999;\ntext-decoration:none;\nfloat:right;\n}\n\n.easyDialog_wrapper .close_btn:hover{\ncolor:#333;\n}\n\n.easyDialog_wrapper .easyDialog_text{\npadding:25px 10px;\nfont-size:13px;\nline-height:22px;\n}\n\n.easyDialog_wrapper .easyDialog_footer{\npadding:0 10px;\n*zoom:1;\n}\n\n.easyDialog_wrapper .easyDialog_footer:after{\ncontent:\'\';\ndisplay:block;\nheight:0;\noverflow:hidden;\nvisibility:hidden;\nclear:both;\n}\n\n.easyDialog_wrapper .btn_highlight,\n.easyDialog_wrapper .btn_normal{\nborder:1px solid;\nborder-radius:2px;\ncursor:pointer;\nfont-family:"Microsoft yahei", Arial;\nfloat:right;\nfont-size:12px;\npadding:0 12px;\nheight:24px;\nline-height:24px;\nmargin-bottom:10px;\n}\n\n.easyDialog_wrapper .btn_highlight{\nbackground:#4787ed;\nbackground:-webkit-gradient(linear,center bottom,center top,from(#4787ed),to(#4d90fe));\nbackground:-moz-linear-gradient(90deg, #4787ed, #4d90fe);\nborder-color:#3079ed;\ncolor:#fff;\n}\n\n.easyDialog_wrapper .btn_normal{\nmargin-left:10px;\nborder-color:#c3c3c3;\nbackground:#ececec;\ncolor:#333;\nbackground:-webkit-gradient(linear,center bottom,center top,from(#ececec),to(#f4f4f4));\nbackground:-moz-linear-gradient(90deg,#ececec,#f4f4f4);\n}\n';
 
-  CSS = '.pinned{\n  padding:0 10px;\n  line-height:50px;\n  opacity:0.7;\n  background: #FC6;\n  border:1px solid #F90;\n  position:fixed;\n  left:10px;\n  bottom:10px;\n  }\n.invisible{\n  display:none;\n  cursor: default;\n  }\nbutton::-moz-focus-inner{ border:0; padding:0; margin:0; }\n\n.easyDialog_wrapper{ width:320px; color:#444; border:3px solid rgba(0,0,0,0); -webkit-border-radius:5px; -moz-border-radius:5px; border-radius:5px; -webkit-box-shadow:0 0 10px rgba(0,0,0,0.4); -moz-box-shadow:0 0 10px rgba(0,0,0,0.4); box-shadow:0 0 10px rgba(0,0,0,0.4); display:none; font-family:"Microsoft yahei", Arial; }\n\n.easyDialog_wrapper .easyDialog_content{ -webkit-border-radius:4px; -moz-border-radius:4px; border-radius:4px; background:#fff; border:1px solid #e5e5e5; }\n\n.easyDialog_wrapper .easyDialog_title{ height:30px; line-height:30px; overflow:hidden; color:#666; padding:0 10px; font-size:14px; border-bottom:1px solid #e5e5e5; background:#f7f7f7; border-radius:4px 4px 0 0; }\n\n.easyDialog_wrapper .close_btn{ font-family:arial; font-size:18px; _font-size:12px; font-weight:700; color:#999; text-decoration:none; float:right; }\n\n.easyDialog_wrapper .close_btn:hover{ color:#333; }\n\n.easyDialog_wrapper .easyDialog_text{ padding:25px 10px; font-size:13px; line-height:22px; }\n\n.easyDialog_wrapper .easyDialog_footer{ padding:0 10px; *zoom:1; }\n\n.easyDialog_wrapper .easyDialog_footer:after{ content:\'\'; display:block; height:0; overflow:hidden; visibility:hidden; clear:both; }\n\n.easyDialog_wrapper .btn_highlight,\n.easyDialog_wrapper .btn_normal{ border:1px solid; border-radius:2px; cursor:pointer; font-family:"Microsoft yahei", Arial; float:right; font-size:12px; padding:0 12px; height:24px; line-height:24px; margin-bottom:10px; }\n\n.easyDialog_wrapper .btn_highlight{ background:#4787ed; background:-webkit-gradient(linear,center bottom,center top,from(#4787ed),to(#4d90fe)); background:-moz-linear-gradient(90deg, #4787ed, #4d90fe); border-color:#3079ed; color:#fff; }\n\n.easyDialog_wrapper .btn_normal{ margin-left:10px; border-color:#c3c3c3; background:#ececec; color:#333; background:-webkit-gradient(linear,center bottom,center top,from(#ececec),to(#f4f4f4)); background:-moz-linear-gradient(90deg,#ececec,#f4f4f4); }\n';
+  cssContainer = document.createElement("style");
 
-  jW("<style>" + CSS + "</style>").appendTo('head');
+  cssContainer.type = "text/css";
+
+  cssContainer.textContent = cssContent;
+
+  try {
+    document.getElementsByTagName("head")[0].appendChild(cssContainer);
+  } catch (_error) {
+    error = _error;
+    console.log(error);
+  }
 
 }).call(this);
--- src/SFW.coffee
+++ src/SFW.coffee
@@ -1,4 +1,3 @@
-jW=jQuery.noConflict()
 fridgeMagnet=
   '''
   <div class="pinned">
@@ -7,50 +6,58 @@ fridgeMagnet=
   '''
 alarmBox=
   '''
-  <div id="alarmBox" class="invisible" >
+  <div id="alarmBox">
     <h6>time for work babe</h6>
     <a id="boxCloseNG" href="javascript:;">OK</a>
   </div>
   '''
 configBox=
   '''
-  <div id="configBox" class="invisible" >
+  <div id="configBox">
     <h6>close me</h6>
     <a id="boxClose" href="javascript:;">OK</a>
     <a id="setClock" href="javascript:;">I wanna work,but not now</a>
   </div>
   '''
-jW("body").append(fridgeMagnet)
-jW("body").append(alarmBox)
-jW("body").append(configBox)
+divToAppend=document.createElement("div")
+divToAppend.innerHTML=fridgeMagnet
+document.body.appendChild(divToAppend)
+
 
 timeForRelax=10 #seconds
 pulseClock=0
-pulseCount=->
-  timeForRelax-=1
-  clearTimeout(pulseClock)
-  jW("#boxOpen").html(timeForRelax)
-  if timeForRelax is 0 #T_T
-    timeoutAlarm()
-  else
-    pulseClock=setTimeout pulseCount,1000
+# pulseCount=->
+  # timeForRelax-=1
+  # clearTimeout(pulseClock)
+  # jW("#boxOpen").html(timeForRelax)
+  # if timeForRelax is 0 #T_T
+    # timeoutAlarm()
+  # else
+    # pulseClock=setTimeout pulseCount,1000
 
-timeoutAlarm=->
+# timeoutAlarm=->
+  # easyDialog.open(
+    # container: "alarmBox"
+  # )
+
+# jW("#boxOpen").click ->
+  # easyDialog.open(
+    # container: "configBox"
+  # )
+document.getElementById("boxOpen").onclick=()->
   easyDialog.open(
-    container: "alarmBox"
+    container:
+      header:"hello"
+      content:configBox
+      noFn:true
   )
 
-jW("#boxOpen").click ->
-  easyDialog.open(
-    container: "configBox"
-  )
+# jW("#boxClose").click ->
+  # easyDialog.close()
 
-jW("#boxClose").click ->
-  easyDialog.close()
+# jW("#setClock").click ->
+  # pulseCount()
+  # easyDialog.close()
 
-jW("#setClock").click ->
-  pulseCount()
-  easyDialog.close()
-
-jW("#boxCloseNG").click ->
+# jW("#boxCloseNG").click ->
   #do nothing
